ce152e10aecb682c3f8cb33cfc1e4eee6051a93a
aib <aibok42@gmail.com>
2016-09-23 19:41:14 +0300

    Increase DHCP lease time to 4h

--- misc/dhcp.sh
+++ misc/dhcp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-dnsmasq -d -p 0 -F 10.0.42.2,10.0.42.2 -O 3,10.0.42.1 -O 6,4.2.2.1,8.8.8.8,8.8.4.4 --dhcp-leasefile=/dev/null
+dnsmasq -d -p 0 -F 10.0.42.2,10.0.42.2,4h -O 3,10.0.42.1 -O 6,4.2.2.1,8.8.8.8,8.8.4.4 --dhcp-leasefile=/dev/null
